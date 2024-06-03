278feeedba159ff24764f727de1c93a99d9a8030
Sonam Jamtsho <sjamtsho@myseneca.ca>
2024-06-02 20:49:59 -0400

    added code to verify terms and condition checkbox is checked while sign up

--- Components/LoginSignup.js
+++ Components/LoginSignup.js
@@ -5,14 +5,16 @@ import { getDefaultCart } from '@/cartlogic';
 import { cartAtom } from '@/store';
 import { useAtom } from 'jotai';
 import { isLoggedInAtom } from '@/store';
+import { Margarine } from 'next/font/google';
 
 const LoginSignup = () => {
   const router = useRouter();
   const [cart, setCart] = useAtom(cartAtom)
-  const [,setIsLoggedIn] = useAtom(isLoggedInAtom)
+  const [, setIsLoggedIn] = useAtom(isLoggedInAtom)
 
   const [state, setState] = useState('Login');
   const [error, setError] = useState(null)
+  const [isChecked, setIsChecked] = useState(false)
   const [userDetails, setUserDetails] = useState({
     name: '',
     email: '',
@@ -32,59 +34,61 @@ const LoginSignup = () => {
       },
       body: JSON.stringify(userDetails),
     }).then(res => res.json()).then(responseData => {
-      if(responseData.success === true){
+      if (responseData.success === true) {
         localStorage.setItem('auth-token', responseData.token);
         setIsLoggedIn(true);
-        getDefaultCart().then(data =>setCart(data)).catch(err=>console.log(err))
+        getDefaultCart().then(data => setCart(data)).catch(err => console.log(err))
         router.push('/');
-      }else{
+      } else {
         setError(responseData)
       }
     }).catch(error => console.log(error));
   }
 
-  const signup =  () => {
-    fetch(`${process.env.NEXT_PUBLIC_API_URL}/user/signup`, {
-      method: 'POST',
-      headers: {
-        Accept: 'application/json',
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(userDetails),
-    }).then(res => res.json()).then(responseData => {
-      if(responseData.success === true){
-        router.push('/');
-        localStorage.setItem('auth-token', responseData.token);
-        setIsLoggedIn(true);
-        setCart(getDefaultCart());
-      }else{
-        console.log(responseData)
-        setError(responseData)
-      }
-    }).catch(error => console.log(error));
+  const signup = () => {
+    if (!isChecked) setError({message:'Please accept the terms'})
+    else {
+      fetch(`${process.env.NEXT_PUBLIC_API_URL}/user/signup`, {
+        method: 'POST',
+        headers: {
+          Accept: 'application/json',
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(userDetails),
+      }).then(res => res.json()).then(responseData => {
+        if (responseData.success === true) {
+          router.push('/');
+          localStorage.setItem('auth-token', responseData.token);
+          setIsLoggedIn(true);
+          setCart(getDefaultCart());
+        } else {
+          console.log(responseData)
+          setError(responseData)
+        }
+      }).catch(error => console.log(error));
+    }
   }
 
-
-return (
-  <div className={styles.loginsignup}>
-    <div className={styles.loginsignup_container}>
-      <h1>{state}</h1>
-      {error && <p style={{color:'red'}}>{error.message+', Try again'}</p>}
-      <div className={styles.loginsignup_fields}>
-        {state === 'Sign Up' ? <input type='text' name='name' value={userDetails.name} onChange={changeHandler} placeholder='Your Name' /> : ''}
-        <input type='email' name='email' value={userDetails.email} onChange={changeHandler} placeholder='Email Address' />
-        <input type='password' name='password' value={userDetails.password} onChange={changeHandler} placeholder='Password' />
+  return (
+    <div className={styles.loginsignup} style={{marginBottom:'40px'}}>
+      <div className={styles.loginsignup_container}>
+        <h1>{state}</h1>
+        {error && <p style={{ color: 'red' }}>{error.message + ', Try again'}</p>}
+        <div className={styles.loginsignup_fields}>
+          {state === 'Sign Up' ? <input type='text' name='name' value={userDetails.name} onChange={changeHandler} placeholder='Your Name' /> : ''}
+          <input type='email' name='email' value={userDetails.email} onChange={changeHandler} placeholder='Email Address' />
+          <input type='password' name='password' value={userDetails.password} onChange={changeHandler} placeholder='Password' />
+        </div>
+        <button onClick={state === 'Login' ? login : signup}>Continue</button>
+        {state === 'Sign Up' ? <><p className={styles.loginsignup_login}>Already have an account? <span onClick={() => setState('Login')}>Login here</span></p>
+          <div className={styles.loginsignup_agree} >
+            <input type='checkbox' name='checkbox' onChange={(e) => setIsChecked(e.target.checked)} id='' />
+            <p>By continuing, I agree to the terms of use and privacy policy</p>
+          </div></>
+          : <p className={styles.loginsignup_login}>Create an account? <span onClick={() => setState('Sign Up')}>Click here</span></p>}
       </div>
-      <button onClick={state==='Login'? login:signup}>Continue</button>
-      {state === 'Sign Up' ? <><p className={styles.loginsignup_login}>Already have an account? <span onClick={() => setState('Login')}>Login here</span></p>
-        <div className={styles.loginsignup_agree}>
-          <input type='checkbox' name='' id='' />
-          <p>By continuing, I agree to the terms of use and privacy policy</p>
-        </div></>
-        : <p className={styles.loginsignup_login}>Create an account? <span onClick={() => setState('Sign Up')}>Click here</span></p>}
     </div>
-  </div>
-)
+  )
 }
 
 export default LoginSignup
